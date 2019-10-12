3a839bdba96a7b5a0474c1046bb2943edff3cfd8
Acerlaorion <ilyamaksakov123@gmail.com>
2019-10-12 19:35:28 +0300

    Задание А и В

--- CourseApp/Program.cs
+++ CourseApp/Program.cs
@@ -6,13 +6,21 @@ namespace CourseApp
     {
         public static double MyFunction(double a, double b, double x)
         {
-            var y = (a * Math.Pow(x, 2)) + (b * x);
+            var y = (Math.Pow(Math.Pow(x - a, 2), 0.33) + Math.Pow(Math.Abs(x + b), 0.2)) / Math.Pow((x * x) - Math.Pow(a + b, 2), 0.11);
             return y;
         }
 
         public static double[] TaskA(double a, double b, double xn, double xk, double dx)
         {
-            return new double[0];
+            int i = 0;
+            var y = new double[(int)((xk - xn) / dx)];
+            for (double x = xn; x < xk; x += dx)
+            {
+                y[i] = MyFunction(a, b, x);
+                i++;
+            }
+
+            return y;
         }
 
         public static double[] TaskB(double a, double b, double[] x)
@@ -28,14 +36,20 @@ namespace CourseApp
 
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            const double a = 2.2;
-            const double b = 3.8;
-            var resSingle = MyFunction(a, b, 4);
-            Console.WriteLine(resSingle);
-            var x = new double[] { 1, 2, 3, 4, 5 };
-            var taskBRes = TaskB(a, b, x);
-            foreach (var item in taskBRes)
+            const double a = 0.8;
+            const double b = 0.4;
+            const double xn = 1.23;
+            const double xk = 7.23;
+            const double dx = 1.2;
+            Console.WriteLine("Задание А:");
+            foreach (var item in TaskA(a, b, xn, xk, dx))
+            {
+                Console.WriteLine($"y = {item}");
+            }
+
+            Console.WriteLine("Задание B:");
+            var x = new double[] { 1.88, 2.26, 3.84, 4.55, -6.21 };
+            foreach (var item in TaskB(a, b, x))
             {
                 Console.WriteLine($"y = {item}");
             }
